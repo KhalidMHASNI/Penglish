b2a3de2f4eb228b2aba66d71b22ec0d691cc0710
KhalidMhasni <kmhasni15@gmail.com>
2022-04-17 16:51:57 +0000

    App is going

--- BiLingoApp/views.py
+++ BiLingoApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+#"def homePage(request):
+#    return render(request,'home.html')
+
+
+#Sign in / up  page
+def SigninupPage(request):
+    return render(request, 'Signinup.html')
