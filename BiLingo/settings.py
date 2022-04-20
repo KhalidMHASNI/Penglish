06760878b082806e36763ef25a0823d148eac2d1
Alami-Simo <med.simo.alami@gmail.com>
2022-04-20 12:11:49 +0200

    Create helloworld.py

--- BiLingo/settings.py
+++ BiLingo/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'BiLingo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
--- BiLingoApp/urls.py
+++ BiLingoApp/urls.py
@@ -1,7 +1,8 @@
 import imp
+from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Signinup/',views.SigninupPage)
+    path('Signinup/',views.SigninupPage),
 ]
