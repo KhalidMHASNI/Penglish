d7408a6a7641a253f48e416cfef9ae1525493790
KhalidMhasni <kmhasni15@gmail.com>
2022-05-24 23:21:18 +0100

    Final test v2

---
+++ BiLingoApp/static/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of these has a different meaning from the rest?",
+    answer: "Tiny",
+    options: [
+      "Huge",
+      "Massive",
+      "Gigantic",
+      "Tiny"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which adjective best completes this sentence? \"Whose ___ new car is that?\"",
+    answer: "Shiny",
+    options: [
+      "Rich",
+      "Shiny",
+      "Slim",
+      "Yummy"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of these is NOT related to taste?",
+    answer: "Humid",
+    options: [
+      "Sour",
+      "Humid",
+      "Sweet",
+      "Bitter"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Andrea lives next door so we _____ see her.",
+    answer: "Often",
+    options: [
+      "Rarely",
+      "Never",
+      "Often",
+      "Occasionally"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
